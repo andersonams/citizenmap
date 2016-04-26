4c2708408b6882c8e0caf038b88bd1ccf1ea590a
Anderson M <Anderson M>
2016-04-26 17:25:04 -0300

    Commit inicial.

---
+++ www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('app.controllers', [])
+  
+.controller('citizenMapCtrl', function($scope) {
+
+})
+   
+.controller('mapaCtrl', function($scope) {
+
+})
+   
+.controller('configuraEsCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('cadastroCtrl', function($scope) {
+
+})
+   
+.controller('principalCtrl', function($scope) {
+
+})
+ 
